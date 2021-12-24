a3f3f603b2528d401e6497124f78751b3702aea9
coffeacloudberry <83429424+coffeacloudberry@users.noreply.github.com>
2021-12-24 19:40:37 +0100

    Fix DeprecationWarning DEP_WEBPACK_DEV_SERVER_HTTPS

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -29,7 +29,7 @@ module.exports = merge(common, {
         compress: false,
         hot: true,
         port: 8080,
-        https: true, // fix CORS issue when connecting to 3rd parties
+        server: "https", // fix CORS issue when connecting to 3rd parties
     },
 
     module: {
