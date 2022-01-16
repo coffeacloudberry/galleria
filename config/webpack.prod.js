d9ed568feb0950545cb9039ce92dd7e34a49987c
coffeacloudberry <83429424+coffeacloudberry@users.noreply.github.com>
2022-01-16 11:50:14 +0100

    Fix build

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -5,7 +5,7 @@
  * https://github.com/taniarascia/webpack-boilerplate/blob/master/LICENSE
  */
 
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin").default;
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const RobotstxtPlugin = require("robotstxt-webpack-plugin");
@@ -151,7 +151,7 @@ module.exports = merge(common, {
     performance: {
         hints: "error",
         // the total amount of emitted files utilized during initial load time
-        maxEntrypointSize: 300 * 1024,
+        maxEntrypointSize: 400 * 1024,
         // the biggest emitted file allowed
         maxAssetSize: 250 * 1024,
     },
