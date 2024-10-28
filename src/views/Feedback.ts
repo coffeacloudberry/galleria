5483fbc6541ea1f396ef8a164e2e493a877ddcde
coffeacloudberry <83429424+coffeacloudberry@users.noreply.github.com>
2024-10-28 19:51:51 +0100

    fix JS-0105

--- src/views/Feedback.ts
+++ src/views/Feedback.ts
@@ -1,10 +1,8 @@
 import thumbsUpOutline from "@/icons/thumbs-up-outline.svg";
 import m from "mithril";
 
-import { LogType } from "../CustomLogging";
 import { feedback } from "../models/Feedback";
 import { t } from "../translate";
-import { toast } from "../utils";
 import Icon from "./Icon";
 
 const SubmitButton: m.Component = {
@@ -21,41 +19,6 @@ const SubmitButton: m.Component = {
 };
 
 export class Feedback implements m.ClassComponent {
-    textUpdate(e: InputEvent) {
-        if (e.data && "+=$|%&#\\[]{}<>".includes(e.data)) {
-            return;
-        }
-        if (e.target) {
-            const target = e.target as HTMLTextAreaElement;
-            feedback.text_content = target.value;
-        }
-    }
-
-    sendFeedback(e: SubmitEvent) {
-        e.preventDefault();
-        feedback.is_sending = true;
-        m.request({
-            method: "POST",
-            url: "https://api.explorewilder.com/api/feedback",
-            body: {
-                message: feedback.text_content,
-                from: location.href,
-            },
-        })
-            .then(() => {
-                feedback.text_content = "";
-                feedback.is_open = false;
-                feedback.is_sending = false;
-                m.redraw();
-                toast(t("feedback.thank-you"));
-            })
-            .catch(() => {
-                feedback.is_sending = false;
-                m.redraw();
-                toast(t("feedback.failed"), LogType.error);
-            });
-    }
-
     view(): m.Vnode {
         if (!feedback.is_open) {
             return m(
@@ -85,14 +48,14 @@ export class Feedback implements m.ClassComponent {
                 "form",
                 {
                     onsubmit: (e: SubmitEvent) => {
-                        this.sendFeedback(e);
+                        feedback.sendFeedback(e);
                     },
                 },
                 [
                     m("textarea[required]", {
                         placeholder: t("feedback.text"),
                         oninput: (e: InputEvent) => {
-                            this.textUpdate(e);
+                            feedback.textUpdate(e);
                         },
                         value: feedback.text_content,
                     }),
